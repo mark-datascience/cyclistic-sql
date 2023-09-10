1d48dc05580e6a4e270e5857186c4d2575ba796d
mark-datascience <144593706+mark-datascience@users.noreply.github.com>
2023-09-11 00:52:37 +0800

    Update processing_cleaning_phase.sql

--- processing_cleaning_phase.sql
+++ processing_cleaning_phase.sql
@@ -17,3 +17,179 @@ CREATE TABLE trip202111(
     end_lng FLOAT NULL,
     member_casual VARCHAR(200) NULL
     );
+
+CREATE TABLE trip202112(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202201(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202202(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202203(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202204(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202205(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202206(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202207(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202208(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202209(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
+
+CREATE TABLE trip202210(
+    ride_id VARCHAR(200) NULL,
+    rideable_Type VARCHAR(200) NULL,
+    started_at TIMESTAMP NULL,
+    ended_at TIMESTAMP NULL,
+    start_station_name VARCHAR(200) NULL,
+    start_station_id VARCHAR(200) NULL,
+    end_station_name VARCHAR(200) NULL,
+    end_station_id VARCHAR(200) NULL,
+    start_lat FLOAT NULL,
+    start_lng FLOAT NULL,
+    end_lat FLOAT NULL,
+    end_lng FLOAT NULL,
+    member_casual VARCHAR(200) NULL
+    );
