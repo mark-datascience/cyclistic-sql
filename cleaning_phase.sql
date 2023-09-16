f3a347b25c2961fb7fce8d568cfe82d656de4fe9
mark-datascience <144593706+mark-datascience@users.noreply.github.com>
2023-09-16 23:54:38 +0800

    Update cleaning_phase.sql

--- cleaning_phase.sql
+++ cleaning_phase.sql
@@ -1,5 +1,11 @@
--------#1 Check if the ride_ids are all unique and have same string lengths
+-------#1 After importing the csv files and combining it into one table, let's verify if we get the same number of total rows
 
+SELECT 
+	COUNT(*) 
+FROM 
+	cyclistic_data.trip_one_year;
+
+-------#2 Check if the ride_ids are all unique and have same string lengths
 SELECT
   COUNT(DISTINCT ride_id) AS unique_ride_id
 FROM
@@ -12,4 +18,14 @@ FROM
 GROUP BY
 	LENGTH(ride_id);
 
+-------#2 Remove leading and trailing spaces at start_station_name and end_station_name
 	
+UPDATE
+	trip_one_year
+SET
+	start_station_name = TRIM(start_station_name);
+
+UPDATE
+	trip_one_year
+SET
+	start_station_name = TRIM(end_station_name);
