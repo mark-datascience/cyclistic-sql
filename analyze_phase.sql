75977491cc9863a12bf47813a837cfa053b5b80f
mark-datascience <144593706+mark-datascience@users.noreply.github.com>
2023-10-21 15:49:20 +0800

    Update analyze_phase.sql

--- analyze_phase.sql
+++ analyze_phase.sql
@@ -1,1 +1,12 @@
+---------Analyzing the data
+
+-------#1 rideable type per membership
+SELECT 
+	rideable_type, member_casual, count(*) AS number_of_rides
+FROM 
+	cyclistic_data.trip_one_year
+GROUP BY
+	rideable_type, member_casual
+ORDER BY
+	member_casual, number_of_rides DESC
 
